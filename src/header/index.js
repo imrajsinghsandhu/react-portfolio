0f2a6b313e3b62e5e1d33b3e7a7946b479f32623
imraj singh sandhu <e0543951@u.nus.edu>
2024-04-26 01:11:12 +0800

    personalise website to imraj
    Add brief history
    Add social links
    Some spelling error fixes

--- src/header/index.js
+++ src/header/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./style.css";
 import { VscGrabber, VscClose } from "react-icons/vsc";
 import { Link } from "react-router-dom";
-import { logotext ,socialprofils } from "../content_option";
+import { logotext ,socialprofiles } from "../content_option";
 import Themetoggle from "../components/themetoggle";
 
 const Headermain = () => {
@@ -52,9 +52,9 @@ const Headermain = () => {
           </div>
           <div className="menu_footer d-flex flex-column flex-md-row justify-content-between align-items-md-center position-absolute w-100 p-3">
             <div className="d-flex">
-            <a href={socialprofils.facebook}>Facebook</a>
-            <a href={socialprofils.github}>Github</a>
-            <a href={socialprofils.twitter}>Twitter</a>
+            <a href={socialprofiles.facebook}>Facebook</a>
+            <a href={socialprofiles.github}>Github</a>
+            <a href={socialprofiles.twitter}>Twitter</a>
             </div>
             <p className="copyright m-0">copyright __ {logotext}</p>
           </div>
